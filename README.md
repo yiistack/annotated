2353b12abaaea29dcfded0fd77398051750ace00
Rustam Mamadaminov <rmamdaminov@gmail.com>
2020-07-08 14:33:10 +0500

    Clean up

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# template
+# Yiistack Annotated
---
+++ tests/AnnotationLoaderTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Yiistack\Annotated\Tests;
+
+use Yiistack\Annotated\AnnotationLoader;
+use PHPUnit\Framework\TestCase;
+
+class AnnotationLoaderTest extends TestCase
+{
+
+    public function testFindProperties()
+    {
+    }
+
+    public function testFindClasses()
+    {
+    }
+
+    public function testFindMethods()
+    {
+    }
+
+    public function testWithTargets()
+    {
+    }
+}
